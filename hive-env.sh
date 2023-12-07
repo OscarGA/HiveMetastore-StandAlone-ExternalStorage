c595657714ec67e66b9d43977dd05a9b8ac4a96d
Oscar Gómez <oscargomez45@hotmail.com>
2023-12-07 10:42:53 -0500

    Environment Hive vars

---
+++ hive-env.sh
@@ -0,0 +1,6 @@
+# Variable to add the AWS auxiliary jars
+export HIVE_AUX_JARS_PATH=/opt/hadoop/share/hadoop/tools/lib/aws-java-sdk-bundle-1.11.271.jar:/opt/hadoop/share/hadoop/tools/lib/hadoop-aws-3.1.0.jar:/opt/hadoop/share/hadoop/tools/lib/aws-java-sdk-s3-1.12.99.jar:/opt/hadoop/share/hadoop/tools/lib/aws-java-sdk-core-1.12.99.jar
+
+# Variables to set AWS credentials
+export AWS_ACCESS_KEY_ID=""
+export AWS_SECRET_ACCESS_KEY=""
